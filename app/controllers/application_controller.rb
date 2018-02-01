bfee2546703a059ad5a78f3e49e64cc910cd227d
Brittany Morris <brittmmendez@gmail.com>
2018-02-01 19:03:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < Sinatra::Base
 
     def current_user
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+      binding.pry
     end
 
   end
