35105dde6487ab6c5e4b3d382a6de08510d30d11
Brittany Morris <brittmmendez@gmail.com>
2018-02-01 19:12:18 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,12 +45,8 @@ class UsersController < ApplicationController
   end
 
   get '/users/:slug' do
-    if logged_in?
-      @user=User.find_by_slug(params[:slug])
+    @user=User.find_by_slug(params[:slug])
       erb :'users/show'
-    else
-      redirect to '/login'
-    end
   end
 
 
